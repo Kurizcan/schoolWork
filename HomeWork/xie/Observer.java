2535e9bc0c81e41aa4ea6429b11aae6a9b1bdcba
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-12-28 21:33:59 +0800

    ssss

--- HomeWork/xie/Observer.java
+++
@@ -1,8 +0,0 @@
-package RealWork;
-
-import java.util.Hashtable;
-
-public interface Observer {
-    public void update(double x, double y, int sign);
-    public void update_get(Hashtable hashtable, int start, int end);
-}
